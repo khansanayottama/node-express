225578633ff8b78de4a0d4017b46a97e65b6a1ab
Khansa Nayottama (Termux) <khansa.nayottama@gmail.com>
2021-01-07 23:33:24 +0700

    Express js Tutorial Indonesia #6 - Membuat Routing

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require('express')
 const app = express()
 
-app.get('', function(request, response) {
-  response.send('Hello World')
+// req = require, res = response
+app.get('', function(req, res) {
+  res.send('Hello World')
 })
 
+app.get('/about', function(req, res) {
+  res.send('About')
+})
+
+app.get('/user', function(req, res) {
+  res.send('Get User')
+})
+
+app.post('/user', function(req, res) {
+  res.send('Post User')
+})
+
+app.put('/user', function(req, res) {
+  res.send('Put User')
+})
+
+app.delete('/user', function(req, res) {
+  res.send('Delete User')
+})
+
+
+// Binds and listens for connections
 app.listen(3000, function() {
   console.log('Server is okay')
 })
